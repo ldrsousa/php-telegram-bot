800dbed7ffea21f5f4b5a0fef4a67bd16cdf78b5
waterblue <leandrodrsousa@gmail.com>
2017-11-16 17:26:27 +0000

    init

--- classes/GuessNumber.php
+++ classes/GuessNumber.php
@@ -24,7 +24,7 @@ class GuessNumber extends Bot
 
 		file_put_contents($this->filename, serialize($this->game));
 
-		return 'Game started!' . chr(10) . 'Lives: ' . $this->game->lives . chr(10) . 'Guess a number between 1 and 50 with /gn &lt;number&gt;';
+		return 'Game started!' . chr(10) . 'Lives: ' . $this->game->lives . chr(10) . 'Guess a number between 1 and 50 with "/gn <number>"';
 	}
 
 	public function stop()
